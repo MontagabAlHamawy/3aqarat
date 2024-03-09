b7712277782f242ace0b4b4458c0d4ac50e3e61d
MontagabAlHamawy <ma0152260@gmail.com>
2024-03-09 19:40:53 +0300

    edit font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,12 +29,6 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* تحديد أيقونة الموقع هنا */}
         <link rel="icon" href="/icon.png" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <body>
         <div>
