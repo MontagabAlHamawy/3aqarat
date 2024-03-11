f415fc32e44d42517f82a71e29709114a26baf43
MontagabAlHamawy <ma0152260@gmail.com>
2024-03-11 10:38:46 +0300

    edit header links

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,14 @@
+"use client"
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { PiUserDuotone, PiMagnifyingGlassDuotone, PiBellSimpleDuotone } from "react-icons/pi";
 
 function Header() {
+  const route = usePathname();
+  const searchActive = route === "/search";
+  const notActive = route === "/notification";
+  const accountActive = route === "/account";
   return (
     <div className="sticky top-0 w-full h-16 bg-sidpar shadow-lg  z-40 xl:pr-20 flex flex-row justify-between items-center">
       <div>
@@ -14,18 +20,18 @@ function Header() {
           className="xl:hidden mr-3 "
         />
         <Link href={"/search"}>
-          <PiMagnifyingGlassDuotone className="text-4xl text-white p-2 bg-body rounded-md cursor-pointer hidden xl:block " />
+          <PiMagnifyingGlassDuotone className={`text-4xl text-white p-2 ${searchActive ? 'bg-accent' : 'bg-body '} rounded-md cursor-pointer hidden xl:block `} />
         </Link>
       </div>
       <div className="mx-2 flex flex-row gap-2">
         <Link href={"/search"}>
-          <PiMagnifyingGlassDuotone className="text-4xl text-white p-2 bg-body rounded-md cursor-pointer xl:hidden" />
+          <PiMagnifyingGlassDuotone className={`text-4xl text-white p-2 ${searchActive ? 'bg-accent' : 'bg-body '} rounded-md cursor-pointer xl:hidden`} />
         </Link>
         <Link href={"/notification"}>
-          <PiBellSimpleDuotone className="text-4xl text-white p-2 bg-body rounded-md cursor-pointer" />
+          <PiBellSimpleDuotone className={` text-4xl text-white p-2 ${notActive ? 'bg-accent' : 'bg-body '} rounded-md cursor-pointer`} />
         </Link>
         <Link href={"/account"}>
-          <PiUserDuotone className="text-4xl text-white p-2 bg-body rounded-md cursor-pointer" />
+          <PiUserDuotone className={`text-4xl text-white p-2 ${accountActive ? 'bg-accent' : 'bg-body '} rounded-md cursor-pointer`} />
         </Link>
       </div>
     </div>
