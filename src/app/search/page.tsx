a6f915cf7e4750ae8088dba5363019fde67e46ac
MontagabAlHamawy <ma0152260@gmail.com>
2024-03-11 09:11:03 +0300

    edit icon

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
+import { PiMagnifyingGlassDuotone } from "react-icons/pi";
 
 export default function Search() {
   return (
-    <div>Search</div>
-  )
+    <div>
+      <div  className="flex flex-row justify-between items-center bg-section mr-[-16px] mt-[-40px] px-10 h-16">
+        <div className="flex flex-row items-center justify-center gap-1">
+          <input type="text" placeholder="اسم المنطقة" className="bg-secondary text-white rounded-md px-1 h-8"/>
+          <button type="submit" className="p-2 bg-body rounded-md">
+            <PiMagnifyingGlassDuotone />
+          </button>
+        </div>
+        
+      </div>
+    </div>
+  );
 }
