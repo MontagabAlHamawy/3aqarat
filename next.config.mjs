2e305e5194a3686fda78f794bede4948182c26cb
unknown <montagabalh@gmail.com>
2024-06-22 19:00:06 +0300

    edit API

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", // توجيه الطلبات إلى API فقط إذا كانت تبدأ بـ /api
+        destination: "https://gruafahim.pythonanywhere.com/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
