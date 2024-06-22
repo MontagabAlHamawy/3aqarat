df31b838d96ff8a66d393f873b13b3eb05898093
unknown <montagabalh@gmail.com>
2024-06-22 11:33:27 +0300

    edit building page

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-        
+  rewrites: [
+    {
+      source: "/api/:path*",
+      destination: "https://gruafahim.pythonanywhere.com/:path*",
+    },
+  ],
 };
 
 export default nextConfig;
