7d8361bb44f25c1a93d27204db13bd3376195efa
unknown <montagabalh@gmail.com>
2024-06-22 18:44:47 +0300

    edit API

--- src/app/buildings/page.tsx
+++ src/app/buildings/page.tsx
@@ -1,21 +1,19 @@
-
 import React, { useState } from "react";
-import { hom, houses } from "../../components/links";
-import Image from "next/image";
-import Link from "next/link";
-import dynamic from "next/dynamic";
 import apiUrl from "@/utils/apiConfig";
 import { toast } from "react-toastify";
-import BuildingFilter from "@/components/BuildingFilter";
 import AllBuildings from "@/components/AllBuildings";
+import axios from "axios";
 
 export default async function Building(props: any) {
-  const response:any = await fetch(`${apiUrl}/properties/`);
-  if (!response.ok) {
-    toast.error("خطاء في جلب البيانات ");
+  try {
+    const response: any = await axios.get(`${apiUrl}/properties/`);
+    if (!response.data) {
+      toast.error("خطاء في جلب البيانات ");
+    }
+    const building = response.data
+
+    return <AllBuildings Building={building} />;
+  } catch (error) {
+    console.log(error);
   }
-  const building = await response.json();
-  return (
-   <AllBuildings Building={building}/>
-  );
 }
